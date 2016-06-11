ac438ed81bb27ab2697a02de5ab0176c723065fc
Tyrox92 <xingox@hotmail.de>
2016-06-11 19:27:30 +0200

    Forgot to Update Version number

--- settings.cpp
+++ settings.cpp
@@ -1,7 +1,7 @@
 #include "settings.h"
 
 using namespace GW2;
-const QString Settings::s_Version = "2.5";
+const QString Settings::s_Version = "3.0";
 const QString Settings::s_protocolVersion = "1.0";
 const QString Settings::s_Product = "Gw2SPECS" + (" " + Settings::s_Version);
 
